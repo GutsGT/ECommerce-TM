2c39addf2f50427c917090725d57ca25dafe3034
Otávio <gutsgt1@gmail.com>
2023-02-05 23:05:14 -0300

    Ultima aula do curso Laravel Intro Tiago Matos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,8 +7,37 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        $products = Product::all();
+    public function index(Request $request){
+        //Métodos para filtrar a pesquisa
+            //Primeiro 
+                //$products = Product::where('name', 'like', '%'.$request->search.'%')->get();
+
+            //Segundo
+                // $products = Product::query();
+                // $products->when($request->search, function($query, $vl){
+                //     $query->where('name', 'like', '%'.$vl.'%');
+                // });
+                // $products = $products->get();
+
+            //Terceiro
+                // $products = Product::query();
+                // if($request->search){
+                //     $query = $products->where('name', 'like', '%'.$request->search.'%')->get();
+                // }
+        //
+
+        $products = Product::query();
+        $products->when($request->search, function($query, $vl){
+            $query->where('name', 'like', '%'.$vl.'%');
+        });
+        $products = $products->get();
+
+        $products = Product::query();
+        // $products->when($request->search, function($query, $vl){
+        //     $query->where('name', 'like', '%'.$vl.'%');
+        // });
+        // $products = $products->get();
+
         return view('home', [
             'products'=>$products
         ]);
